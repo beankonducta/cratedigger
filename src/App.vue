e6c4dbc837e286fe94070ca89896a8a23232dc2d
patrick andrews <patrickxandrews@gmail.com>
2021-12-08 16:29:11 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,211 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <div v-if="data.length > 0">
+      <div class="record" v-for="d in data" v-bind:key="d.id">
+        <img v-bind:src="d.basic_information.cover_image">
+        {{ d.basic_information.artists[0].name }}
+        {{ d.basic_information.title }}
+      </div>
+    </div> -->
+    <Canvas v-if="data.length > 0" :records="data"/>
+    <div v-if="data.length === 0">
+      <h1>Loading...</h1>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import Canvas from "./components/Canvas.vue";
+require("dotenv").config();
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Canvas },
+  methods: {
+    getReleases() {
+      axios
+        .get(
+          `https://api.discogs.com/users/beankonducta/collection/folders/0/releases?key=${process.env.VUE_APP_KEY}&secret=${process.env.VUE_APP_SECRET}&per_page=5`
+        )
+        .then((res) => {
+          this.data = this.mapData(res.data.releases.sort((a, b) =>
+            a.basic_information.artists[0].name >
+            b.basic_information.artists[0].name
+              ? 1
+              : -1
+          ));
+        });
+    },
+    mapData(d) {
+      let newData = [];
+      d.forEach((val) => {
+        newData.push({
+          title: val.basic_information.title,
+          artist: val.basic_information.artists[0].name,
+          cover: val.basic_information.cover_image,
+          year: val.basic_information.year,
+          id: val.id,
+          hasSleeve: false,
+        });
+
+      });
+      return newData;
+    },
+  },
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted() {
+    this.getReleases();
+  },
+};
 </script>
 
 <style>
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol,
+ul {
+  list-style: none;
+}
+blockquote,
+q {
+  quotes: none;
+}
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 1000px;
+}
+
+.record {
+  width: 25%;
+  height: 10%;
+  display: flex;
+}
+
+img {
+  width: 50%;
+  height: 100%;
+  background: black;
+  margin: 1%;
 }
 </style>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,214 @@
+<template>
+  <div id="cratedigger">
+    <div id="cratedigger-canvas"></div>
+    <div id="cratedigger-loading">
+      <div class="info-container">
+        <div class="vertical-center">
+          <span class="loading-label">Loading records...</span>
+        </div>
+      </div>
+    </div>
+    <div id="cratedigger-info">
+      <div class="info-container">
+        <div class="vertical-center">
+          <div id="cratedigger-record-artist"></div>
+          <div id="cratedigger-record-title"></div>
+          <div id="cratedigger-record-cover"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import cratedigger from "../assets/scripts/cratedigger.js";
+
+export default {
+  name: "Canvas",
+  components: {},
+  props: ["records"],
+  data() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null,
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  created() {},
+  computed: {},
+  methods: {
+    init() {
+      var self = this;
+      cratedigger.init({
+        debug: false,
+        elements: {
+          rootContainer: document.getElementById("cratedigger"),
+          canvasContainer: document.getElementById("cratedigger-canvas"),
+          loadingContainer: document.getElementById("cratedigger-loading"),
+          infoContainer: document.getElementById("cratedigger-info"),
+        },
+        onInfoPanelOpened() {
+          self.fillInfoPanel(cratedigger.getSelectedRecord());
+        },
+
+        onInfoPanelClosed() {},
+      });
+
+      cratedigger.loadRecords(this.records, true, () => {
+        
+      });
+    },
+    fillInfoPanel(record) {
+      const titleContainer = document.getElementById(
+        "cratedigger-record-title"
+      );
+      const artistContainer = document.getElementById(
+        "cratedigger-record-artist"
+      );
+      const coverContainer = document.getElementById(
+        "cratedigger-record-cover"
+      );
+      if (record.data.title) {
+        titleContainer.innerHTML = record.data.title;
+      }
+
+      if (record.data.artist) {
+        artistContainer.innerHTML = record.data.artist;
+      }
+
+      if (record.data.cover) {
+        coverContainer.style.backgroundImage = "url(" + record.data.cover + ")";
+      }
+    },
+  },
+  watch: {},
+};
+</script>
+
+<style scoped>
+#cratedigger {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  overflow: hidden;
+}
+
+#cratedigger-canvas canvas.grab {
+  cursor: -webkit-grabbing;
+  cursor: -moz-grabbing;
+}
+
+#cratedigger-loading {
+  background-color: #111111;
+  z-index: 10;
+  height: 100%;
+  width: 100%;
+  transition: opacity 1s;
+}
+
+#cratedigger-info {
+  background-color: rgba(17, 17, 17, 0.9);
+  z-index: 5;
+  display: block;
+  transition: opacity 0.6s;
+}
+
+#cratedigger-record-title {
+  color: #fefefe;
+  font-size: 40px;
+  line-height: 45px;
+  font-weight: 700;
+  text-transform: uppercase;
+}
+
+#cratedigger-record-artist {
+  color: #fefefe;
+  font-size: 26px;
+  font-weight: 700;
+}
+
+#cratedigger-record-cover {
+  display: inline-block;
+  width: 400px;
+  height: 400px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  margin: 25px 0 50px;
+}
+
+.info-container {
+  display: table;
+  width: 100%;
+  height: 100%;
+}
+
+.vertical-center {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+}
+
+.loading-label {
+  color: #fefefe;
+  font-size: 50px;
+  line-height: 50px;
+  font-weight: 700;
+  text-transform: uppercase;
+}
+
+#bottom-bar {
+  position: absolute;
+  z-index: 8;
+  background-color: rgba(17, 17, 17, 0.5);
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 60px;
+  width: 100%;
+  display: table;
+  color: #fff;
+  overflow: hidden;
+  transition: all 0.3s ease-in-out;
+  table-layout: fixed;
+}
+
+#bottom-bar.closed {
+  bottom: -60px;
+}
+
+#bottom-bar .bottom-bar-item {
+  display: table-cell;
+  text-align: center;
+  text-transform: uppercase;
+  line-height: 60px;
+  font-size: 30px;
+  transition: all 0.15s ease-in-out;
+  overflow: hidden;
+  color: #fff;
+  cursor: pointer;
+}
+
+#bottom-bar .bottom-bar-item:hover {
+  background-color: #fff;
+  color: #111;
+}
+
+.noselect {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
